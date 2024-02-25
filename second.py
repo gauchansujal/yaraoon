f9970cfb53a4da1ed80759467faa1b8bdead4c1d
mirajgansi <mirajgansi122@gmail.com>
2024-02-25 16:08:49 +0545

    commit

--- second.py
+++ second.py
@@ -10,7 +10,7 @@ root.geometry("1200x1000")
 
 
 
-frame = Frame(root, bg="#024c6e", highlightbackground="black", highlightthickness=2, bd=2,height=100)
+frame = Frame(root, bg="#53D3D1", highlightbackground="black", highlightthickness=2, bd=2,height=100)
 frame.pack(expand=TRUE, fill=BOTH)
 
 
@@ -24,7 +24,7 @@ desired_height = 50
 resized_image = image.subsample(int(image.width()/desired_width), int(image.height()/desired_height))
 
 
-label = Label(frame, bg="#024c6e",  width=desired_width, height=100, image = resized_image, anchor=W)
+label = Label(frame, bg="#53D3D1",  width=desired_width, height=100, image = resized_image, anchor=W)
 label.pack(side = TOP, expand=False, fill=BOTH)
 label.pack_propagate(False)
 label.pack(pady=7)
@@ -59,15 +59,15 @@ button.pack(side=RIGHT, padx=(0,30),pady=25)
 
 
 
-second_label = Label(frame,  width=500, height=500, bg="#024c6e",anchor=CENTER)
+second_label = Label(frame,  width=500, height=500, bg="#53D3D1",anchor=CENTER)
 second_label.pack(side = TOP, expand=False, fill=BOTH) 
 
-message=Message(second_label,text="Choose  Bus",bg="#024c6e",fg="black", width=500,font=("Poppins", 40, "bold"))
+message=Message(second_label,text="Choose  Bus",bg="#53D3D1",fg="black", width=500,font=("Poppins", 40, "bold"))
 message.pack (side=TOP,padx=100)
 
 # adding calendar  to the frame
 
-message=Message(second_label,text="Date",bg="#024c6e",font=("Poppins", 16, "bold"))
+message=Message(second_label,text="Date",bg="#53D3D1",font=("Poppins", 16, "bold"))
 message.pack(side=LEFT,padx=5, pady=1)
 
 cal=DateEntry(second_label ,selectmode='day')
@@ -86,76 +86,76 @@ def change():
         print("User clicked Cancel")
 
 
-# CREATINGG BUTTON
+#-------------------------------------CREATINGG BUTTON--------------------------------------------
 
 desired_width2 = 150
 desired_height2 = 50 
 
-frame1 = Frame(frame, width= desired_width, height= desired_height ,bg="#024c6e", highlightbackground="black", highlightthickness=0)
-frame1.pack( padx=(50,50), pady=100)
+frame1 = Frame(frame, width= desired_width, height= desired_height ,bg="#53D3D1", highlightbackground="black", highlightthickness=0)
+frame1.pack( padx=(50,50), pady=75)
 
 
-message_label= Label(frame1, width=desired_width, bg="#024c6e",height=desired_height,borderwidth=1,anchor=CENTER)
-message_label.pack(side = TOP, expand=False, fill=BOTH)
+message_label= Label(frame1, width=desired_width, bg="#53D3D1",height=desired_height,borderwidth=1)
+message_label.pack(side = TOP, anchor=CENTER,expand=False, fill=BOTH)
 
-bus_message=Message(message_label,text="Bus type", font=("Poppins", 20, "bold"),fg="black",bg="#024c6e", width=desired_width)
-bus_message.pack(side=LEFT,padx=(350,550),pady=8)
+bus_message=Message(message_label,text="Bus type", font=("Poppins", 20, "bold"),fg="black",bg="#53D3D1", width=250)
+bus_message.pack(side=LEFT,padx=(265,300),pady=8)
 
-departure_message=Message(message_label,text="Departure", font=("Poppins", 20, "bold"),fg="black",bg="#024c6e", width=500)
+departure_message=Message(message_label,text="Departure", font=("Poppins", 20, "bold"),fg="black",bg="#53D3D1", width=250)
 departure_message.pack(side=LEFT, padx=0, pady=8, anchor=CENTER)
 
-fare_message=Message(message_label,text="Fare", font=("Poppins", 20, "bold"),fg="black",bg="#024c6e", width=desired_width)
-fare_message.pack(side=RIGHT,padx=(150),pady=8)
+fare_message=Message(message_label,text="Fare", font=("Poppins", 20, "bold"),fg="black",bg="#53D3D1", width=250)
+fare_message.pack(side=RIGHT,padx=(250),pady=8)
 
 
-# creating first label for button
-button_label=Label(frame1, width=desired_width, height=desired_height,bg="#FBECEB",highlightbackground="black",highlightthickness=1)
-button_label.pack(side = TOP, expand=False, fill=BOTH)
+# ----------------------------------------creating first label for button----------------------------------
+button_label=Label(frame1, width=desired_width, height=desired_height,bg="#002447",highlightbackground="black",highlightthickness=1)
+button_label.pack(side = TOP)
  
-bus_button=Button(button_label,text="Tourist travels ", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus_button=Button(button_label,text="Tourist travels ", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus_button.pack(side=LEFT,padx=(35,0),pady=1)
 
-bus2_button=Button(button_label,text="6:30 PM", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus2_button=Button(button_label,text="6:30 PM", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus2_button.pack(side=LEFT,padx=0,pady=8)
 
-bus3_button=Button(button_label,text="rs:650", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
-bus3_button.pack(side=RIGHT,padx=(0,35),pady=8)
+bus3_button=Button(button_label,text="rs:650", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
+bus3_button.pack(side=LEFT,padx=(0,35),pady=8)
 
-# creating second label for button
-second_button= Label(frame1, width=desired_width, height=desired_height,bg="#FBECEB",highlightbackground="black",highlightthickness=1)
-second_button.pack(side = TOP, expand=False, fill=BOTH)
+# ---------------------------------------creating second label for button--------------------------------------------
+second_button= Label(frame1, width=desired_width, height=desired_height,bg="#002447",highlightbackground="black",highlightthickness=1)
+second_button.pack(side = TOP)
 
-bus4_button=Button(second_button,text="Legend Gorkha ", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus4_button=Button(second_button,text="Legend Gorkha ", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus4_button.pack(side=LEFT,padx=(35,0),pady=8)
 
-bus5_button=Button(second_button,text="4:00 PM", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus5_button=Button(second_button,text="4:00 PM", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus5_button.pack(side=LEFT,padx=0,pady=8)
 
-bus6_button=Button(second_button,text="rs:10000", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus6_button=Button(second_button,text="rs:7500", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus6_button.pack(side=RIGHT,padx=(0,35),pady=8)
 
-# creating third label for button
-third_button= Label(frame1, width=desired_width, height=desired_height,bg="#FBECEB",highlightbackground="black",highlightthickness=1)
-third_button.pack(side = TOP, expand=False, fill=BOTH)
+# ----------------------------------------creating third label for button------------------------------------------------------
+third_button= Label(frame1, width=desired_width, height=desired_height,bg="#002447",highlightbackground="black",highlightthickness=1)
+third_button.pack(side = TOP)
 
-bus7_button=Button(third_button,text="Nepal travels ", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus7_button=Button(third_button,text="Nepal travels ", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus7_button.pack(side=LEFT,padx=(35,0),pady=8)
 
-bus8_button=Button(third_button,text="12:30 PM", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus8_button=Button(third_button,text="12:30 PM", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus8_button.pack(side=LEFT,padx=0,pady=8)
 
-bus9_button=Button(third_button,text="rs.700", bg='#FBECEB', fg='black', height=5, width=100,borderwidth=0, activebackground="#FBECEB")
+bus9_button=Button(third_button,text="rs.700", bg='#002447', fg="white",font=("Poppins", 14,"bold") ,height=3, width=30,borderwidth=0, activebackground="#002447")
 bus9_button.pack(side=RIGHT,padx=(0,35),pady=8)
 
-fourth_button= Label(frame1,bg='#024c6e', width=desired_width, height=desired_height)
+fourth_button= Label(frame1,bg='#53D3D1', width=desired_width, height=desired_height)
 fourth_button.pack(side = TOP, expand=False, fill=BOTH)
-
+#-------------------------buying ticket------------------------------------------------
 def buy():
     messagebox.showinfo("Ticket","Are you sure you want to book this Bus")
     root.destroy()
     import bus
-buy_button=Button( fourth_button,text="Buy ticket", bg='#53D3D1', fg='black', height=5, width=30, padx=5, pady=5, command=buy)
-buy_button.pack(side=RIGHT)
+buy_button=Button( fourth_button,text="Buy ticket", bg='red', fg="black", font=("Poppins", 14,"bold") ,height=3, width=16,  command=buy)
+buy_button.pack(side=RIGHT,anchor=CENTER)
 
 #---------------------------------------creating a button for change in plan-------------------------
 def change():
@@ -168,7 +168,7 @@ def change():
     else:
         print("User clicked Cancel")
 
-change_button = Button(fourth_button, text="change in plan?", bg="#FEB249",height=5, width=30,cursor = "hand2",padx=5, pady=5,command=change,activebackground="red")
+change_button = Button(fourth_button, text="change in plan?", bg="#FEB249",cursor = "hand2",font=("Poppins", 14,"bold") ,height=3, width=16,command=change,activebackground="red")
 change_button.pack(side=RIGHT)
 
 root.mainloop()
