38a4446f2c34e4358d3346e2170f99d59f2a50b9
mirajgansi <mirajgansi122@gmail.com>
2024-02-25 18:26:23 +0545

    update

--- boarding_pass.py
+++ boarding_pass.py
@@ -29,8 +29,10 @@ def logout():
     if result:
         print("User clicked OK")
         root.destroy()
-    import log_in2
-    
+        import log_in2
+    else:
+        print("user clicked cancle")
+
 
 button = Button(label, text="Log out",height=10,width=15,bg="#0094FF",cursor = "hand2",font=('Inter',10,"bold"),command=logout)
 button.pack(side=RIGHT,pady=25)
@@ -57,71 +59,78 @@ f = Frame(frame, bg="#53D3D1",width= 1295, height=500, highlightbackground="blac
 f.pack( side= TOP, fill=Y)
 
 label = Label(f, bg='#53D3D1',  width=500, height=300, anchor=CENTER)
-label.pack(side = TOP, expand=False, fill=BOTH,pady=(10,150))
+label.pack(side = TOP, expand=False, fill=BOTH,pady=(40,0))
 
 message_label=Label(label,bg='black',  width=500, height=5, anchor=N)
 message_label.pack(side=TOP, fill=BOTH,)
 
-ticket_message=Message(message_label,text="Ticket Details ",font=('Inka',29,'bold'), background="black",fg="white",width=500,anchor=N)
+ticket_message=Message(message_label,text="Ticket Details ", font=("Poppins", 20,"bold"), background="black",fg="white",width=500,anchor=N)
 ticket_message.pack(padx=1,fill=BOTH)
 
 
-detail_label=Label(label,bg='#43F4FF', height=100,width=50, highlightbackground="black", highlightthickness=2, bd=0)
+detail_label=Label(label,bg='white', height=100,width=50, highlightbackground="black", highlightthickness=2, bd=0)
 detail_label.pack(side=LEFT, fill=Y) 
 
 
-date=Label(detail_label,text="Date:",bg='#43F4FF',font=('Helvetica', 12)).grid(row=1, column=1)
-date_message=Message(detail_label,text="Ticket Details ",font=('Helvetica',12,'underline'), background="#43F4FF",fg="black",width=500).grid(row=1, column=2)
+date=Label(detail_label,text="Date:",bg='white', font=("Poppins", 14,"bold")).grid(row=1, column=1)
+date_message=Message(detail_label,text="Ticket Details ", font=("Poppins", 14,"bold",'underline'), background="white",fg="black",width=500).grid(row=1, column=2)
 
 
-bus=Label(detail_label,text="Bus No. :",bg='#43F4FF',font=('Helvetica', 12)).grid(row=2, column=1)
-bus_message=Message(detail_label,text="Ticket Details ",font=('Helvetica',12,'underline'), background="#43F4FF",fg="black",width=500).grid(row=2, column=2)
+bus=Label(detail_label,text="Bus No. :",bg='white', font=("Poppins", 14,"bold")).grid(row=2, column=1)
+bus_message=Message(detail_label,text="Ticket Details ", font=("Poppins", 14,"bold",'underline'), background="white",fg="black",width=500).grid(row=2, column=2)
 
-age=Label(detail_label,text="Age:",bg='#43F4FF',font=('Helvetica', 12)).grid(row=3, column=1)
-age_message=Message(detail_label,text="Ticket Details ",font=('Helvetica',12,'underline'), background="#43F4FF",fg="black",width=500).grid(row=3, column=2)
+age=Label(detail_label,text="Age:",bg='white', font=("Poppins", 14,"bold")).grid(row=3, column=1)
+age_message=Message(detail_label,text="Ticket Details ", font=("Poppins", 14,"bold",'underline'), background="white",fg="black",width=500).grid(row=3, column=2)
 
 #--------------------------------------here we make new tiket label------------------------------------------------------
 
-fare=Label(detail_label,text="Fare Rs. :",bg='#43F4FF',font=('Helvetica', 12)).grid(row=4, column=1)
-fare_message=Message(detail_label,text="Ticket Details ",font=('Helvetica',12,'underline'), background="#43F4FF",fg="black",width=500).grid(row=4, column=2)
+fare=Label(detail_label,text="Fare Rs. :",bg='white', font=("Poppins", 14,"bold")).grid(row=4, column=1)
+fare_message=Message(detail_label,text="Ticket Details ", font=("Poppins", 14,"bold",'underline'), background="white",fg="black",width=500).grid(row=4, column=2)
 
-serial=Label(detail_label,text="S. NO:",bg='#43F4FF',font=('Helvetica', 12)).grid(row=5, column=1)
-serial_message=Message(detail_label,text="Ticket Details ",font=('Helvetica',12,'underline'), background="#43F4FF",fg="black",width=500).grid(row=5, column=2)
+serial=Label(detail_label,text="S. NO:",bg='white', font=("Poppins", 14,"bold")).grid(row=5, column=1)
+serial_message=Message(detail_label,text="Ticket Details ", font=("Poppins", 14,"bold",'underline'), background="white",fg="black",width=500).grid(row=5, column=2)
 
-pass_label=Label(label,bg='#43F4FF',  width=desired_width, height=100,highlightbackground="black", highlightthickness=2, bd=0)
+pass_label=Label(label,bg='white',  width=desired_width, height=100,highlightbackground="black", highlightthickness=2, bd=0)
 pass_label.pack(side=RIGHT, fill=Y,expand=YES)
 
 
-pass_message=Message(pass_label,text="BOARDING PASS ",font=('Imprint MT Shadow',20,'bold'), background="#43F4FF",fg="black",width=500).grid(row=1, column=2)
+pass_message=Message(pass_label,text="Bus Ticket ",font=('poppins',20,'bold'), background="white",fg="black",width=500).grid(row=1, column=2)
 
-name_message=Message(pass_label,text="Name",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=2, column=1)
+name_message=Message(pass_label,text="Name", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=2, column=1)
+name2_message=Message(pass_label,text=":Name", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=3, column=1)
 
+ticket_number_message=Message(pass_label,text="Ticket ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=2, column=2)
+ticket2_message=Message(pass_label,text=":Name", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=3, column=2)
 
-ticket_number_message=Message(pass_label,text="Ticket ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=2, column=2)
+fare2_base_message=Message(pass_label,text="Fare base ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=2, column=3)
+fare2_base_message=Message(pass_label,text=":Fare base ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=3, column=3)
 
+issue2_message=Message(pass_label,text="Isused by ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=2, column=4)
+issue2_message=Message(pass_label,text=":Isused by ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=3, column=4)
 
-fare_base_message=Message(pass_label,text="Fare base ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=2, column=3)
+from_message=Message(pass_label,text="From ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=4, column=1)
+from2_message=Message(pass_label,text=":From ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=5, column=1)
 
-issue_message=Message(pass_label,text="Isused by ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=2, column=4)
+Bus_date_message=Message(pass_label,text="Date ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=4, column=2)
+Bus2_date_message=Message(pass_label,text=":Date ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=5, column=2)
 
+bus_time_message=Message(pass_label,text="Time ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=4, column=3)
+bus2_time_message=Message(pass_label,text=":Time ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=5, column=3)
 
-from_message=Message(pass_label,text="From ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=3, column=1)
+bus_seat_message=Message(pass_label,text="Seat ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=4, column=4)
+bus2_seat_message=Message(pass_label,text=":Seat ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=5, column=4)
 
+bus_ticket_message=Message(pass_label,text="Ticket number ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=4, column=4)
+bus2_ticket_message=Message(pass_label,text=":Ticket number ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=5, column=4)
 
-Bus_date_message=Message(pass_label,text="Date ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=3, column=2)
+to_message=Message(pass_label,text="To ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=6, column=1)
+to_message=Message(pass_label,text="To ", font=(":Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=7, column=1)
 
+reaching_date_message=Message(pass_label,text="Date ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=6, column=2)
+reaching_date_message=Message(pass_label,text=":Date ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=7, column=2)
 
-bus_time_message=Message(pass_label,text="Time ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=3, column=3)
-
-bus_seat_message=Message(pass_label,text="Seat ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=3, column=4)
-
-bus_ticket_message=Message(pass_label,text="Ticket number ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=3, column=4)
-
-to_message=Message(pass_label,text="To ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=4, column=1)
-
-reaching_date_message=Message(pass_label,text="Date ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=4, column=2)
-
-reaching_time_message=Message(pass_label,text="Seat ",font=('Inka',12), background="#43F4FF",fg="#1814D4",width=500).grid(row=4, column=3)
+reaching_time_message=Message(pass_label,text="Seat ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=6, column=3)
+reaching_time_message=Message(pass_label,text=":Seat ", font=("Poppins", 14,"bold"), background="white",fg="black",width=500).grid(row=7, column=3)
 
 #-------------------------------here wwe are adding qr photo to scan  --------------------------
 image_path_2= r"qr.png"
@@ -132,29 +141,29 @@ desired_height2 = 100
 
 resized_image2 = image2.subsample(int(image2.width()/desired_width2), int(image2.height()/desired_height2))
 
-qr_img= Label(pass_label, image = resized_image2,background="#43F4FF" ).grid(row=5 ,column=4)
-
-btn_label=Label(frame,bg="#53D3D1",width=100,height=100,anchor=SE)
-btn_label.pack(side=RIGHT,padx=70,pady=100)
+qr_img= Label(pass_label, image = resized_image2,background="white" ).grid(row=9 ,column=4)
 #-------------------asking user how do they want to save passs--------------------------
-def email():
-    result=messagebox.askyesno('Email', "Do You Want to send pass in your email")
-    if result:
-        print("User clicked OK")
-    
-    else:
-        print("User clicked Cancel")
-
-send_button=Button(btn_label, text="Send it to email",font=("Helvetica",16,"bold"),background="#1893ED",fg="white",command=email)
-send_button.pack(side=LEFT,padx=0,pady=50)
-
+btn_label=Label(f,bg="#53D3D1",width=100,height=100)
+btn_label.pack(side=RIGHT)
 def download():
     result=messagebox.askyesnocancel("Download","Do You Want To Download The Pass?")
     if result:
         print("User clicked OK")
     else:
         print("User clicked Cancel")
-download_button=Button(btn_label,text="Download",font=("Helvetica",16,"bold"),background="#1893ED",fg="white")
-download_button.pack(side=RIGHT,padx=10,pady=50)
+download_button=Button(btn_label,text="Download",bg="#FEB249",cursor = "hand2",font=("Poppins", 14,"bold") ,height=3, width=16,command=download,activebackground="red")
+download_button.pack(side=RIGHT,anchor=CENTER)
+
+def email():
+    result=messagebox.askyesnocancel('Email', "Do You Want to send pass in your email")
+    if result:
+        print("User clicked OK")
+    
+    else:
+        print("User clicked Cancel")
+
+send_button=Button(btn_label, text="Send it to email", bg="#FEB249",cursor = "hand2",font=("Poppins", 14,"bold") ,height=3, width=16,command=email,activebackground="red")
+send_button.pack(side=RIGHT)
+
 
 root.mainloop()
