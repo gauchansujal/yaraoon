c248c91fcd05dbff12ef7883d5fd68a5258988bd
mirajgansi <mirajgansi122@gmail.com>
2024-02-27 14:19:27 +0545

    commit update

--- payment_method.py
+++ payment_method.py
@@ -33,21 +33,21 @@ def login2():
 button = Button(label, text="sign in",height=10,width=15,bg="#0094FF",cursor = "hand2",font=('poppins',10,"bold"),command=login2)
 button.pack(side=RIGHT,pady=25)
 
-
-def popup(): 
-    messagebox.showinfo("contac us", "987635925924")
-b3_button=Button(label, text="contact us",height=10,width=15, bg="#D3D3D3",cursor = " hand2",font=('poppins',10,"bold"), command=popup)
+def message_popup(): 
+    messagebox.showinfo("contac us", "Please get in touch with us right away if you have any issues with this app. Contact us at\n +977 985511133.")
+b3_button=Button(label, text="contact us",height=10,width=15, bg="#D3D3D3",cursor = " hand2",font=('Inter',10,"bold"), command=message_popup)
 b3_button.pack(side=RIGHT,  padx=(0,30),pady=25)
 
 def aboutus_popup():
-    messagebox.showinfo("About Us","we are bus travel")
-b2_button = Button(label, text="about us", height=10,width=15,bg="#D3D3D3",cursor = "  hand2",font=('poppins',10,"bold"),command=aboutus_popup)
+    messagebox.showinfo("About Us","We are travel bus; we are the first travel agency in Nepal where you may purchase a ticket to reserve a bus and travel at an affordable price throughout Nepal. By offering practical and reasonably priced bus reservation services, Travel Bus is transforming the travel sector in Nepal and facilitating individuals' exploration of the natural beauty of our nation.")
+b2_button = Button(label, text="about us", height=10,width=15,bg="#D3D3D3",cursor = "  hand2",font=('Inter',10,"bold"),command=aboutus_popup)
 b2_button.pack(side=RIGHT, padx=(0,30),pady=25)
 
 def service_showinfo():
-    messagebox.showinfo("Service","We provide the best services for you")
-button = Button(label, text="service",height=10,width=15, bg="#D3D3D3",cursor = "  hand2",font=('poppins',10,"bold"), command=service_showinfo)
+    messagebox.showinfo("Service","At Travel Bus, we take pride in offering the best bus reservation service in Nepal. Our software is designed to cater to the needs of both locals and visitors, providing a seamless and convenient way to schedule buses for their travels across the country.")
+button = Button(label, text="service",height=10,width=15, bg="#D3D3D3",cursor = "  hand2",font=('Inter',10,"bold"), command=service_showinfo)
 button.pack(side=RIGHT, padx=(0,30),pady=25)
+
 
 #--------------------------creating a frame for payment--------------------------------------  
 
